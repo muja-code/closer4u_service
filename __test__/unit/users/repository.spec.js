7b50b193c821b9f9555d973822e2a3e3785ae9cb
Bo LEE <85431891+boleesystem@users.noreply.github.com>
2023-01-05 16:41:18 +0900

    feat : controller, repository 유닛테스트, 회원가입 프론트 추가

--- __test__/unit/users/repository.spec.js
+++ __test__/unit/users/repository.spec.js
@@ -1,26 +1,59 @@
-const UserRepository = require('../../../repositories/users/users');
+const UserRepository = require("../../../repositories/users/users");
 
 let MockUserModel = {
+  create: jest.fn(),
   findOne: jest.fn(),
 };
 
 let mockUserId = 1;
 
-let mockError = new Error('mock Error');
+let mockError = new Error("mock Error");
 
 let userRepository = new UserRepository(MockUserModel);
 
-describe('3계층 아키텍처 패턴 users 리포지토리 unit 테스트', () => {
+describe("3계층 아키텍처 패턴 users 리포지토리 unit 테스트", () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
 
-  test('users 리포지토리 findOne Method 성공', async () => {
+  test("users 리포지토리 create Method 성공", async () => {
     const userReturnValue = {
-      account_id: 'TestId1',
-      nickname: 'Nickname1',
+      member: 1,
+      account_id: "testId1",
+      encryptPassword: "testPassword",
+      nickname: "nickname1",
+      phone: "123465",
+      address: "testAddress",
+      point: 1000000,
+    };
+
+    userRepository.userModel.create = jest.fn(() => userReturnValue);
+
+    const user = await userRepository.createUser(mockUserId);
+
+    expect(userRepository.userModel.create).toHaveBeenCalledTimes(1);
+    expect(user).toEqual(userReturnValue);
+  });
+
+  test("users 리포지토리 create Method 실패", async () => {
+    const userReturnValue = {
+      errorMessage: mockError.message,
+    };
+
+    userRepository.userModel.create = jest.fn(() => userReturnValue);
+
+    const user = await userRepository.createUser(mockUserId);
+
+    expect(userRepository.userModel.create).toHaveBeenCalledTimes(1);
+    expect(user).toEqual(userReturnValue);
+  });
+
+  test("users 리포지토리 findOne Method 성공", async () => {
+    const userReturnValue = {
+      account_id: "TestId1",
+      nickname: "Nickname1",
       phone: 123465,
-      address: 'Address1',
+      address: "Address1",
       point: 1000000,
     };
 
@@ -32,7 +65,7 @@ describe('3계층 아키텍처 패턴 users 리포지토리 unit 테스트', () 
     expect(user).toEqual(userReturnValue);
   });
 
-  test('users 리포지토리 findOne Method 실패', async () => {
+  test("users 리포지토리 findOne Method 실패", async () => {
     const userReturnValue = {
       errorMessage: mockError.message,
     };
